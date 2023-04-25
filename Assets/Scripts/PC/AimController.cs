64b0542bd1680214080f2cdf9e0321176e27c4a9
Ian <58906578+Ian-Tc@users.noreply.github.com>
2023-04-25 17:31:00 +0530

    Added PC Aim Controller
    Notes:
    Displays the aim curve
    Does not broadcast nor expose curve initial velocity vector (can obviously be done in a minute if needed, to avoid the same computation for the Shooting)
    
    + rearranged a few folders

---
+++ Assets/Scripts/PC/AimController.cs
@@ -0,0 +1,135 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AimController : MonoBehaviour
+{
+	[SerializeField] private Transform origin;
+	[SerializeField] private float maxInitialVelocity = 11.5f;
+	[SerializeField] private float minInitialVelocity = 6f;
+	[SerializeField] private float deadZone = 0.05f;
+	[SerializeField] private int smoothFrames = 40;
+	[SerializeField] private LayerMask collisionLayerMask;
+	[SerializeField] private LineRenderer curveRendererPreHit, curveRendererPostHit;
+	[SerializeField] private float curveTimeStep = 0.025f;
+
+	Queue<Vector2> smoothedInputs;
+	Camera cam;
+
+	// Start is called before the first frame update
+	void Start()
+	{
+		if (smoothFrames < 1) smoothFrames = 1;
+		smoothedInputs = new Queue<Vector2>(smoothFrames);
+
+		cam = Camera.main;
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		Vector2 inputs = GetAxesInputs();
+
+		if (Mathf.Abs(inputs.x) > deadZone || Mathf.Abs(inputs.y) > deadZone)
+		{
+			float alpha = Mathf.Atan2(inputs.y, inputs.x);
+			float inputMag = Mathf.Clamp01(inputs.magnitude);
+			float v0 = inputMag / (1f - deadZone) * (maxInitialVelocity - minInitialVelocity) + minInitialVelocity;
+			float g = Physics2D.gravity.y;
+
+			Vector3[] curvePreHit, curvePostHit;
+			(curvePreHit, curvePostHit) = GetBallisticCurves(v0, alpha, g);
+
+			curveRendererPreHit.enabled = true;
+			curveRendererPreHit.positionCount = curvePreHit.Length;
+			curveRendererPreHit.SetPositions(curvePreHit);
+			curveRendererPostHit.enabled = true;
+			curveRendererPostHit.positionCount = curvePostHit.Length;
+			curveRendererPostHit.SetPositions(curvePostHit);
+		}
+		else
+		{
+			curveRendererPreHit.enabled = false;
+			curveRendererPostHit.enabled = false;
+		}
+	}
+
+	Vector2 GetAxesInputs()
+	{
+		smoothedInputs.Enqueue(new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical")));
+		if (smoothedInputs.Count > smoothFrames)
+		{
+			smoothedInputs.Dequeue();
+		}
+
+		Vector2 ret = new Vector2();
+		foreach (Vector2 v in smoothedInputs)
+		{
+			ret += v;
+		}
+		ret /= smoothedInputs.Count;
+		return ret;
+	}
+
+	Rect GetCamBoundaries()
+	{
+		Vector2 bottomLeft = cam.ViewportToWorldPoint(Vector2.zero);
+		Vector2 topRight = cam.ViewportToWorldPoint(Vector2.one);
+
+		return new Rect(bottomLeft, topRight - bottomLeft);
+	}
+
+	//Vector3[] GetBallisticCurve(float v0, float alpha, float g)
+	//{
+	//	int maxPointsCount = Mathf.CeilToInt(maxCurveDuration / curveTimeStep);
+	//	Vector3[] curvePoints = new Vector3[maxPointsCount];
+
+	//	float t = 0f;
+
+	//	for (int i = 0; i < maxPointsCount; i++)
+	//	{
+	//		float x = v0 * Mathf.Cos(alpha) * t + origin.position.x;
+	//		float y = 0.5f * g * t * t + v0 * Mathf.Sin(alpha) * t + origin.position.y;
+	//		curvePoints[i] = new Vector3(x, y);
+	//		t += curveTimeStep;
+	//	}
+
+	//	return curvePoints;
+	//}
+
+	(Vector3[], Vector3[]) GetBallisticCurves(float v0, float alpha, float g)
+	{
+		List<Vector3> curvePointsPreHit = new List<Vector3>();
+		List<Vector3> curvePointsPostHit = new List<Vector3>();
+
+		bool hasHit = false;
+
+		Rect camBoundaries = GetCamBoundaries();
+		float x = camBoundaries.center.x, y = camBoundaries.center.y;
+
+		for (float t = 0f; !(y < camBoundaries.yMin || x < camBoundaries.xMin || x > camBoundaries.xMax); t += curveTimeStep)
+		{
+			x = v0 * Mathf.Cos(alpha) * t + origin.position.x;
+			y = 0.5f * g * t * t + v0 * Mathf.Sin(alpha) * t + origin.position.y;
+			Vector2 point = new Vector2(x, y);
+
+			if (!hasHit)
+			{
+				Collider2D coll = Physics2D.OverlapPoint(point, collisionLayerMask);
+				if (coll != null)
+				{
+					curvePointsPostHit.Add(point);
+					hasHit = true;
+				}
+				curvePointsPreHit.Add(point);
+			}
+			else
+			{
+				curvePointsPostHit.Add(point);
+			}
+		}
+
+		return (curvePointsPreHit.ToArray(), curvePointsPostHit.ToArray());
+	}
+
+}
