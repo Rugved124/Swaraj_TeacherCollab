df1d1ee562d7e6c547fea4389e3889a28bbb9f1b
Rugved124 <rugved.marathe@rubikaindia.in>
2023-05-10 11:20:14 +0530

    Layer Added and Changed
    Pickups Layer changed
    Activable Object changed
    Script Added
    Drawbridge finished

--- Assets/Scripts/Rugved/ActivableObject.cs
+++ Assets/Scripts/Rugved/ActivableObject.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ActivableObjects : MonoBehaviour
+public abstract class ActivableObject : MonoBehaviour
 {
     Lever lever;
     DrawBridge bridge;
--- Assets/Scripts/Rugved/Lever.cs
+++ Assets/Scripts/Rugved/Lever.cs
@@ -3,34 +3,21 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Lever : InteractiveObject
-{   public DrawBridge bridge;
-	// Start is called before the first frame update
-	private bool _activated = false;
+{
+	[SerializeField] ActivableObject activableTarget;
+
+	private bool activated = false;
+
 
 	public override void OnHitByArrow()
 	{
-		base.OnHitByArrow();
-
-		Debug.Log("Arrow hit the lever!");
-
-		//Trigger the door or something here
-	}
-	private void OnTriggerEnter2D(Collider2D collision)
-	{
-		Arrow arrow = collision.GetComponent<Arrow>();
-		if (arrow != null && !_activated)
-		{
-			// Arrow hit the lever for the first time
-			_activated = true;
-
-			// Trigger the door to open
-			bridge.Activate();
-			Debug.Log("Khul Ja Sim Sim Sim");
-		}
-	}
-
-	public void Activate()
-	{
+		if (activated) return;
 		
+		activableTarget.Activate();
+
+		activated = true;
+
+		GetComponent<Animator>().SetTrigger("activate");
 	}
+
 }
