b2cc696ab4f9276196f35179ebc6dc36743fbd50
Ian <58906578+Ian-Tc@users.noreply.github.com>
2023-05-17 13:55:05 +0530

    Added editable field to set no-controls duration in TutorialTimeline

--- Assets/Scripts/Tutorial/TutorialTimeline.cs
+++ Assets/Scripts/Tutorial/TutorialTimeline.cs
@@ -5,36 +5,44 @@ using UnityEngine.Playables;
 
 public class TutorialTimeline : MonoBehaviour
 {
-    PlayableDirector director;
-    GameManager gameManager;
-    bool hasPlayed;
+	[SerializeField] float durationNoControls = 2f;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        director = GetComponent<PlayableDirector>();
-        gameManager = FindAnyObjectByType<GameManager>();
-    }
+	PlayableDirector director;
+	GameManager gameManager;
+	bool hasPlayed;
 
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
+	// Start is called before the first frame update
+	void Start()
+	{
+		director = GetComponent<PlayableDirector>();
+		gameManager = FindAnyObjectByType<GameManager>();
+	}
+
+	private void OnTriggerEnter2D(Collider2D collision)
+	{
 		if (hasPlayed) return;
 
-        if (collision.gameObject.layer == LayerMask.NameToLayer("PC"))
-        {
-            gameManager.SwitchOffInputManager();    
-            director.Play();
-            hasPlayed = true;
-        }
-    }
+		if (collision.gameObject.layer == LayerMask.NameToLayer("PC"))
+		{
+			StartCoroutine(CoPreventControls());
+			director.Play();
+			hasPlayed = true;
+		}
+	}
 
-    // Update is called once per frame
-    void Update()
-    {
-        if (director.state != PlayState.Playing && hasPlayed)
-        {
-            //hasPlayed = false;
-            gameManager.SwitchOnInputManager(); 
-        } 
-    }
+	// Update is called once per frame
+	void Update()
+	{
+		if (director.state != PlayState.Playing && hasPlayed)
+		{
+			//hasPlayed = false;
+		}
+	}
+
+	IEnumerator CoPreventControls()
+	{
+		gameManager.SwitchOffInputManager();
+		yield return new WaitForSeconds(durationNoControls);
+		gameManager.SwitchOnInputManager();
+	}
 }
