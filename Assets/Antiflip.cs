716952cca8fb4314fa893c532bb16b0f51fbd2d1
adibhai445 <129141283+adibhai445@users.noreply.github.com>
2023-05-18 13:42:15 +0530

    Fixed enmey and tut levels
    fixed enemies and text tut

---
+++ Assets/Antiflip.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Antiflip : MonoBehaviour
+{
+    
+    void Update()
+    {
+        transform.rotation = Quaternion.identity;
+    }
+}
+    
